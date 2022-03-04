c0efe69e73e5e7e327d3ce626df056001e1476ea
Ivan Sergeev <ahrejlok2007@ya.ru>
2022-03-04 15:58:02 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import MainPage from './Main-page';
+
+function App()  { 
+
+
+  return (      
+      <MainPage />
+  )
+}
+
+export default App;
---
+++ src/Main-page.js
@@ -0,0 +1,14 @@
+import {Fragment} from 'react'
+const MainPage = () => {
+
+    return (
+      <Fragment>
+        <div className="main-page"></div>
+        <div className='qr'></div>
+        </Fragment>
+          );
+  
+ 
+
+}
+  export default MainPage   
